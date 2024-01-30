7aa205a4cda90cd8890451192566697d91f48339
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-01-30 15:09:35 +0900

    feat: 240130 실습

---
+++ mathJs.js
@@ -0,0 +1,6 @@
+function powerJs(a, b) {
+  return a ** b;
+}
+
+powerJs(5, 3);
+powerJs("5", "3");
---
+++ mathTs.ts
@@ -0,0 +1,6 @@
+function powerTs(a: number, b: number) {
+  return a ** b;
+}
+
+powerTs(5, 3);
+powerTs("5", "3");
